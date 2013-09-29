d691a2858ba3b60c76def0c00e108e013449f027
Dash Kiwing <dash.kiwing@gmail.com>
2013-09-29 16:09:01 +0700

    nambah thumbail

--- application/controllers/feed.php
+++ application/controllers/feed.php
@@ -1,24 +1,23 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-class Feed extends Controller 
-{
+class Feed extends CI_Controller {
 
-    function Feed()
-    {
-        parent::Controller();
-        $this->load->model('posts_model', '', TRUE);
+
+	function __construct() {
+		parent::__construct();
         $this->load->helper('xml');
+		$this->load->model('mdl_content','',TRUE);
     }
     
     function index()
     {
         $data['encoding'] = 'utf-8';
         $data['feed_name'] = 'nurulfajar.org';
-        $data['feed_url'] = 'http://www.nurulfajar.com';
+        $data['feed_url'] = 'http://www.nurulfajar.org';
         $data['page_description'] = 'Nurul Fajar merupakan';
         $data['page_language'] = 'en-ca';
         $data['creator_email'] = 'dash.kiwing@gmail.com';
-        $data['post'] = $this->mdl_content->article_news_records()->result();
+        $data['posts'] = $this->mdl_content->article_records()->result();
         header("Content-Type: application/rss+xml");
         $this->load->view('feed/rss', $data);
     }
--- application/views/feed/rss.php
+++ application/views/feed/rss.php
@@ -1,6 +1,4 @@
-<?php 
-echo '<?xml version="1.0" encoding="utf-8"?>' . "\n";
-?>
+<?= '<?xml version="1.0" encoding="utf-8"?>' . "\n";?>
 <rss version="2.0"
     xmlns:dc="http://purl.org/dc/elements/1.1/"
     xmlns:sy="http://purl.org/rss/1.0/modules/syndication/"
@@ -11,31 +9,31 @@ echo '<?xml version="1.0" encoding="utf-8"?>' . "\n";
     <channel>
     
     <title><?php echo $feed_name; ?></title>
-
     <link><?php echo $feed_url; ?></link>
     <description><?php echo $page_description; ?></description>
     <dc:language><?php echo $page_language; ?></dc:language>
     <dc:creator><?php echo $creator_email; ?></dc:creator>
 
-    <dc:rights>Copyright <?php echo gmdate("Y-m-d", time()); ?></dc:rights>
-    <admin:generatorAgent rdf:resource="http://www.codeigniter.com/" />
+    <dc:rights>Copyright <?php echo gmdate("Y", time()); ?></dc:rights>
+    <admin:generatorAgent rdf:resource="http://www.futureleaf.co.id/" />
 
-    <?php foreach($posts as $entry): ?>
+    <?php foreach($posts as $articles): ?>
     
         <item>
 
-          <title><?php echo xml_convert($entry->post_title); ?></title>
-          <link><?php echo site_url('blog/post/' . $entry->url_title) ?></link>
-          <guid><?php echo site_url('blog/post/' . $entry->url_title) ?></guid>
+          <title><?php echo xml_convert($articles->name_content); ?></title>
+          <link><?php echo site_url('front/articles/' . $articles->id_content) ?></link>
+          <guid><?php echo site_url('front/articles/' . $articles->id_content) ?></guid>
 
           <description><![CDATA[
-      <?= str_replace('/img/post_resources/', base_url() . 'img/post_resources/', $entry->post_body); ?>
+      <?= base_url() . 'uploads/thumbs/thumb_', $articles->picture_content; ?>
       ]]></description>
-      <pubDate><?php echo date ('r', $entry->post_date);?></pubDate>
+      <pubDate><?php echo $articles->cd_content; ?></pubDate>
         </item>
 
         
     <?php endforeach; ?>
     
-    </channel></rss> 
+    </channel>
+</rss> 
 
